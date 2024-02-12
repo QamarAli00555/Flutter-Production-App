4f78a39ade2f3f9cf218c77fa1c4e4656de06d51
Qamar Ali <qamaralizaine786@gmail.com>
2024-02-13 01:22:25 +0500

    Update firebase_options.dart
    Updated Firebase key

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,12 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 
 FirebaseOptions firebaseOptions = Platform.isAndroid
     ? const FirebaseOptions(
-        apiKey: 'AIzaSyD53WLIENaLqF0denKHhn3rsrPwtCzNMrI',
-        appId: '1:1004479452704:android:66ff2c31b773b914608e45',
-        messagingSenderId: '1004479452704',
-        projectId: 'productionapp-1b418')
+        apiKey: Your_API_Key,
+        appId: Your_APP_Id,
+        messagingSenderId: Your_Sender_Id,
+        projectId: Your_Project_Id,)
     : const FirebaseOptions(
-        apiKey: 'AIzaSyD9go2lf8RhmvhIh588WGdVxZ2DidQU1WU',
-        appId: '1:1004479452704:ios:9fbf637b05b25dde608e45',
-        messagingSenderId: '1004479452704',
-        projectId: 'productionapp-1b418');
+        apiKey: Your_API_Key,
+        appId:  Your_APP_Id,
+        messagingSenderId:  Your_Sender_Id,
+        projectId: Your_Project_Id);
